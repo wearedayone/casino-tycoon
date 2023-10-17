f18d7337fd0d41fa8fb3cc24facf18c06221d02e
jack <jack.nguyen@weareday.one>
2023-10-17 10:43:30 +0700

    feat(app): init using cra-template-pwa

--- app/src/index.js
+++ app/src/index.js
@@ -7,8 +7,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-const theme = createTheme();
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Black Ops One', cursive",
+  },
+});
 
 // default: 10s for stale time, 10m for cache time
 const queryClient = new QueryClient({
@@ -37,3 +43,13 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
