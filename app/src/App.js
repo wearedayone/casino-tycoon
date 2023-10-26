42493da947d29805d479f5e9af4167727d8ca4bd
Ally Tran <h.anhtr15@gmail.com>
2023-10-26 15:44:39 +0700

    fix: only play audio when in game screen

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,7 @@
-import { useEffect, useState } from 'react';
-
 import Navigations from './navigations';
-import useSettingStore from './stores/setting.store';
 
 const App = () => {
-  const [userHasInteracted, setUserHasInteracted] = useState(false);
-  const [audio] = useState(new Audio('/audios/bg.mp3'));
-  const sound = useSettingStore((state) => state.sound);
-
-  useEffect(() => {
-    const play = audio.play;
-    audio.addEventListener('ended', play);
-    return () => {
-      audio.removeEventListener('ended', play);
-    };
-  }, []);
-
-  useEffect(() => {
-    userHasInteracted && sound === 'on' ? audio.play().catch((e) => {}) : audio.pause();
-  }, [userHasInteracted, sound]);
-
-  return (
-    <div onClick={() => setUserHasInteracted(true)}>
-      <Navigations />
-    </div>
-  );
+  return <Navigations />;
 };
 
 export default App;
--- app/src/navigations/MainRoutes.js
+++ app/src/navigations/MainRoutes.js
@@ -1,13 +1,18 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import useBackgroundMusic from '../hooks/useBackgroundMusic';
 
 const MainRoutes = () => {
+  const { setUserHasInteracted } = useBackgroundMusic();
+  
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <div onClick={() => setUserHasInteracted(true)}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
   );
 };
 
