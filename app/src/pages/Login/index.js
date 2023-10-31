598c50b2061ca4198b28887ac434ced67abecfb3
jack <jack.nguyen@weareday.one>
2023-10-31 10:38:18 +0700

    feat: new login ui

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -1,35 +1,73 @@
+import { useState } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import { usePrivy } from '@privy-io/react-auth';
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const Login = () => {
   const { login } = usePrivy();
+  const [loading, setLoading] = useState(false);
+
+  const addCssForPrivyDialog = () => {
+    const existedTag = document.querySelector('#privy-css');
+    if (existedTag) return;
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+      #privy-dialog { display: none }
+    `;
+    style.id = 'privy-css';
+
+    document.head.appendChild(style);
+  };
+
+  const onClickLoginBtn = async () => {
+    if (loading) return;
+    setLoading(true);
+    addCssForPrivyDialog();
+    login();
+    await delay(200);
+    const privyDialog = document.querySelector('#privy-dialog');
+    const buttons = [...privyDialog.querySelectorAll('button')];
+    const twitterLoginButton = buttons.at(-1);
+    twitterLoginButton?.click();
+    setLoading(false);
+  };
 
   return (
-    <Box
-      minHeight="100vh"
-      p={2}
-      display="flex"
-      flexDirection="column"
-      justifyContent="flex-end"
-      gap={4}
-    >
-      <Typography fontSize={64} fontWeight={600} align="center" lineHeight={1}>
-        GANGSTER ARENA
+    <Box minHeight="100vh" p={2} display="flex" flexDirection="column" justifyContent="center" gap={8}>
+      <Typography fontSize={48} fontWeight={700} fontFamily="'Inknut Antiqua', serif" align="center" lineHeight={1.25}>
+        Gangster <br /> Arena
       </Typography>
-      <Typography fontSize={18} fontWeight={600} align="center">
-        Work your way to be the top Godfather!
-      </Typography>
-      <Button fullWidth variant="contained" onClick={login}>
-        Login
-      </Button>
-      <Typography
-        fontSize={14}
-        fontWeight={600}
-        align="center"
-        sx={{ cursor: 'pointer' }}
-      >
-        {'Check out our privacy policy -->'}
-      </Typography>
+      <Box display="flex" flexDirection="column" gap={1}>
+        <Typography fontSize={14} fontWeight={600}>
+          Connect with
+        </Typography>
+        <Box p={2} border="1px solid black" borderRadius={2} display="flex" flexDirection="column" gap={1}>
+          <Typography fontSize={14} fontWeight={600}>
+            Social
+          </Typography>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={onClickLoginBtn}
+            sx={{
+              bgcolor: '#019EF7',
+              boxShadow: 'none',
+              height: '40px',
+              '&:hover': {
+                boxShadow: 'none',
+              },
+            }}>
+            <img src="/images/icons/twitter.png" alt="twitter" width={24} />
+          </Button>
+        </Box>
+        <Box display="flex" alig nItems="center" justifyContent="center" gap={1}>
+          <img src="/images/icons/privy.png" alt="privy" width={20} />
+          <Typography fontSize={12} color="grey" align="center">
+            Protected by Privy
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 };
