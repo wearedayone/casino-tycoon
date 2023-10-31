991da52a64399bdc405ff9e3aed466ebc3ce3de7
jack <jack.nguyen@weareday.one>
2023-10-31 14:08:34 +0700

    feat: add troubleshoot text in login page

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -34,39 +34,61 @@ const Login = () => {
   };
 
   return (
-    <Box minHeight="100vh" p={2} display="flex" flexDirection="column" justifyContent="center" gap={8}>
-      <Typography fontSize={48} fontWeight={700} fontFamily="'Inknut Antiqua', serif" align="center" lineHeight={1.25}>
-        Gangster <br /> Arena
-      </Typography>
-      <Box display="flex" flexDirection="column" gap={1}>
-        <Typography fontSize={14} fontWeight={600}>
-          Connect with
+    <Box minHeight="100vh" p={2} display="flex" flexDirection="column" justifyContent="center">
+      <Box flex={1} display="flex" flexDirection="column" justifyContent="center" gap={4}>
+        <Typography
+          fontSize={48}
+          fontWeight={700}
+          fontFamily="'Inknut Antiqua', serif"
+          align="center"
+          lineHeight={1.25}>
+          Gangster <br /> Arena
         </Typography>
-        <Box p={2} border="1px solid black" borderRadius={2} display="flex" flexDirection="column" gap={1}>
+        <Box display="flex" flexDirection="column" gap={1}>
           <Typography fontSize={14} fontWeight={600}>
-            Social
+            Connect with
           </Typography>
-          <Button
-            fullWidth
-            variant="contained"
-            onClick={onClickLoginBtn}
-            sx={{
-              bgcolor: '#019EF7',
-              boxShadow: 'none',
-              height: '40px',
-              '&:hover': {
+          <Box p={2} border="1px solid black" borderRadius={2} display="flex" flexDirection="column" gap={1}>
+            <Typography fontSize={14} fontWeight={600}>
+              Social
+            </Typography>
+            <Button
+              fullWidth
+              variant="contained"
+              onClick={onClickLoginBtn}
+              sx={{
+                bgcolor: '#019EF7',
                 boxShadow: 'none',
-              },
-            }}>
-            <img src="/images/icons/twitter.png" alt="twitter" width={24} />
-          </Button>
+                height: '40px',
+                '&:hover': {
+                  boxShadow: 'none',
+                },
+              }}>
+              <img src="/images/icons/twitter.png" alt="twitter" width={24} />
+            </Button>
+          </Box>
+          <Box display="flex" alignItems="center" justifyContent="center" gap={1}>
+            <img src="/images/icons/privy.png" alt="privy" width={20} />
+            <Typography fontSize={12} color="grey" align="center">
+              Protected by Privy
+            </Typography>
+          </Box>
         </Box>
-        <Box display="flex" alig nItems="center" justifyContent="center" gap={1}>
-          <img src="/images/icons/privy.png" alt="privy" width={20} />
-          <Typography fontSize={12} color="grey" align="center">
-            Protected by Privy
-          </Typography>
-        </Box>
+      </Box>
+      <Box display="flex" flexDirection="column" justifyContent="flex-end" alignItems="center">
+        <Typography
+          fontSize={14}
+          fontWeight={600}
+          align="center"
+          sx={{
+            '& span': {
+              cursor: 'pointer',
+              color: '#019EF7',
+              textDecoration: 'underline',
+            },
+          }}>
+          Need help? Troubleshoot <span>here</span>
+        </Typography>
       </Box>
     </Box>
   );
