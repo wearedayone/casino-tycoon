dc3e5d5eea9a965113b8a59ba2995920a928e828
jack <jack.nguyen@weareday.one>
2023-11-13 14:35:12 +0700

    feat: add blur bg for popup

--- app/src/pages/Game/components/Popup.js
+++ app/src/pages/Game/components/Popup.js
@@ -7,6 +7,7 @@ class Popup extends Phaser.GameObjects.Container {
   constructor(scene, img = 'popup') {
     super(scene, 0, 0);
 
+    this.background = scene.add.rectangle(0, 0, configs.width, configs.height, 0x000000, 0.4).setOrigin(0, 0);
     this.popup = scene.add.image(configs.width / 2, configs.height / 2, img).setOrigin(0.5, 0.5);
     this.closeButton = new Button(
       scene,
@@ -16,6 +17,7 @@ class Popup extends Phaser.GameObjects.Container {
       'button-close-pressed',
       () => this.destroy(true)
     );
+    this.add(this.background);
     this.add(this.popup);
     this.add(this.closeButton);
   }
