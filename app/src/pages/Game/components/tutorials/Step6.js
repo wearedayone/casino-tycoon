3b1d554a02708f9f6c74628912b1305ed2f48ba2
jack <jack.nguyen@weareday.one>
2023-12-21 10:37:26 +0700

    feat: tutorial coin animation

--- app/src/pages/Game/components/tutorials/Step6.js
+++ app/src/pages/Game/components/tutorials/Step6.js
@@ -9,8 +9,12 @@ const { width, height } = configs;
 const y = 2600;
 
 class Step6 extends Phaser.GameObjects.Container {
+  scene;
+
   constructor(scene, onNext) {
     super(scene, 0, 0);
+    this.scene = scene;
+    this.coinIcon = scene.add.image(width / 2, y, 'icon-coin-glowing');
 
     this.setVisible(false);
 
@@ -21,13 +25,38 @@ class Step6 extends Phaser.GameObjects.Container {
     this.add(this.claimButtonLight);
 
     this.claimButton = new Button(scene, width / 2, y, 'tutorial-6-claim-btn', 'tutorial-6-claim-btn', () => {
-      onNext();
+      this.startCoinAnimation();
       scene.game.events.emit('simulator-claim-completed', { amount: 15000 });
+      scene.tutorial.setVisible(false);
+      setTimeout(() => {
+        scene.tutorial.setVisible(true);
+        onNext();
+      }, 3000);
     });
     this.add(this.claimButton);
 
     this.arrow = scene.add.image(width / 2, 2500, 'tutorial-arrow-down').setOrigin(0.5, 1);
     this.add(this.arrow);
+  }
+
+  startCoinAnimation() {
+    this.scene.tweens.add({
+      targets: this.coinIcon,
+      x: [
+        width / 2 + 80,
+        width / 2 + 120,
+        width / 2 + 120,
+        width / 2 + 100,
+        width / 2 + 50,
+        width / 2,
+        width / 2 - 140,
+      ],
+      y: [y, height * 0.7, height / 2, height * 0.35, height * 0.2, height * 0.15, 250],
+      duration: 2400,
+      ease: 'Cubic.out',
+      interpolation: 'bezier', // turn the points into curve
+      hideOnComplete: true,
+    });
   }
 }
 
