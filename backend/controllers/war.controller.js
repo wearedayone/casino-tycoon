d8807ff8083a3f1b59737c81614cb3baa95ad7e0
jack <jack.nguyen@weareday.one>
2023-12-31 18:10:40 +0700

    feat: api get list user to attack

--- backend/controllers/war.controller.js
+++ backend/controllers/war.controller.js
@@ -1,9 +1,21 @@
-import { getLatestWar } from '../services/warSnapshot.service.js';
+import { getLatestWar, getUsersToAttack } from '../services/warSnapshot.service.js';
 import logger from '../utils/logger.js';
 
 export const getLastestWarSnapshot = async (req, res) => {
   try {
     const result = await getLatestWar(req.userId);
+    return res.status(200).send(result);
+  } catch (err) {
+    console.error(err);
+    logger.error(err.message);
+    return res.status(400).send(err.message);
+  }
+};
+
+export const getUserListToAttack = async (req, res) => {
+  try {
+    const { page = '0', limit = '50', search = '' } = req.query;
+    const result = await getUsersToAttack({ page: Number(page), limit: Number(limit), search });
     return res.status(200).send(result);
   } catch (err) {
     console.error(err);
--- backend/routes/war.route.js
+++ backend/routes/war.route.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import { getLastestWarSnapshot } from '../controllers/war.controller.js';
+import { getLastestWarSnapshot, getUserListToAttack } from '../controllers/war.controller.js';
 import auth from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.get('/latest', auth, getLastestWarSnapshot);
+
+router.get('/users-to-attack', auth, getUserListToAttack);
 
 export default router;
