3e379c59513eecc83ff5d77921f5ed4e46451b54
jack <jack.nguyen@weareday.one>
2023-12-31 22:37:37 +0700

    feat: api to get user to attack detail

--- backend/routes/war.route.js
+++ backend/routes/war.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { getLastestWarSnapshot, getUserListToAttack } from '../controllers/war.controller.js';
+import { getLastestWarSnapshot, getUserListToAttack, getUserAttackDetail } from '../controllers/war.controller.js';
 import auth from '../middlewares/auth.middleware.js';
 
 const router = Router();
@@ -8,5 +8,7 @@ const router = Router();
 router.get('/latest', auth, getLastestWarSnapshot);
 
 router.get('/users-to-attack', auth, getUserListToAttack);
+
+router.get('/users-to-attack/:id', getUserAttackDetail);
 
 export default router;
