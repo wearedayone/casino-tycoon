75b2059b2c6e18d9e8f9b29d16d2fb3a339607fb
jack <jack.nguyen@weareday.one>
2024-01-12 14:59:51 +0700

    feat: deploy deposit contract to eth && base goerli

--- contract/GameV4/scripts/deployDepositLayerL1.js
+++ contract/GameV4/scripts/deployDepositLayerL1.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const { ethers } = require('hardhat');
 
 async function main() {
-  const adminWallet = '29d2bf86f2d42bd92f45663619ffe26f4ae6c4eade595d58327bfdf37bbe729c';
+  const adminWallet = '0xd88A3D28c2a04b39A6c6521F8D8037C3c3B5e96c';
 
   const depositLayerL1Contract = await ethers.getContractFactory('DepositLayerL1');
   const depositLayerL1 = await depositLayerL1Contract.deploy(adminWallet);
+
   const depositLayerL1Address = await depositLayerL1.getAddress();
-  console.log(`depositLayerL1 is deployed to ${depositLayerL1Address}`);
   console.log(
     `Run this script to verify this contract: \n npx hardhat verify --network [your_network] ${depositLayerL1Address} "${adminWallet}"`
   );
