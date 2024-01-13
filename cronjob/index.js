37d436366f4ed5e87a23c47bd42784252e93a06a
jack <jack.nguyen@weareday.one>
2024-01-13 17:03:05 +0700

    feat: cron job update prices

--- cronjob/index.js
+++ cronjob/index.js
@@ -1,7 +1,16 @@
 import cron from 'node-cron';
 import { updateFIATPrice } from './tasks/updateFIATPrice.js';
+import calculateAvgPrice from './tasks/calculateAvgPrice.js';
 
-// everyday at 1am
 cron.schedule('*/15 * * * *', function () {
   updateFIATPrice();
 });
+
+// everyday at 1am
+cron.schedule(
+  '0 1 * * *',
+  () => {
+    calculateAvgPrice();
+  },
+  { timezone: 'Etc/UTC' }
+);
---
+++ cronjob/tasks/calculateAvgPrice.js
@@ -0,0 +1,73 @@
+import moment from 'moment';
+import admin, { firestore } from '../configs/admin.config.js';
+
+const calculateAvgPrice = async () => {
+  const now = moment().utc();
+  const endTime = now.startOf('day').toDate().getTime();
+  const startTime = endTime - 24 * 60 * 60 * 1000;
+  const endTimestamp = admin.firestore.Timestamp.fromMillis(endTime);
+  const startTimestamp = admin.firestore.Timestamp.fromMillis(startTime);
+  const today = now.subtract(1, 'day').format('DD-MM-YYYY');
+  const date = now.subtract(1, 'day').format('DD/MM');
+
+  console.log({
+    now,
+    today,
+    startTime,
+    endTime,
+    startDate: new Date(startTime),
+    endDate: new Date(endTime),
+  });
+
+  const workerTxns = await firestore
+    .collection('transaction')
+    .where('type', '==', 'buy-worker')
+    .where('status', '==', 'Success')
+    .where('createdAt', '>=', startTimestamp)
+    .where('createdAt', '<', endTimestamp)
+    .get();
+
+  console.log(`Worker txn ${today} count: ${workerTxns.size}`);
+
+  const allWorkerPrices = workerTxns.docs.reduce((result, doc) => [...result, ...doc.data().prices], []);
+  if (allWorkerPrices.length) {
+    const totalWorkerPrices = allWorkerPrices.reduce((total, price) => total + price, 0);
+    const avgWorkerPrice = Number((totalWorkerPrices / allWorkerPrices.length).toFixed(1));
+    await firestore
+      .collection('worker-prices')
+      .doc(today)
+      .set({ date, value: avgWorkerPrice, createdAt: admin.firestore.FieldValue.serverTimestamp() });
+  } else {
+    await firestore
+      .collection('worker-prices')
+      .doc(today)
+      .set({ date, value: 0, createdAt: admin.firestore.FieldValue.serverTimestamp() });
+  }
+
+  const buildingTxns = await firestore
+    .collection('transaction')
+    .where('type', '==', 'buy-building')
+    .where('status', '==', 'Success')
+    .where('createdAt', '>=', startTimestamp)
+    .where('createdAt', '<=', endTimestamp)
+    .get();
+
+  console.log(`Building txn ${today} count: ${buildingTxns.size}`);
+
+  const allBuildingPrices = buildingTxns.docs.reduce((result, doc) => [...result, ...doc.data().prices], []);
+  if (allBuildingPrices.length) {
+    const totalBuildingPrices = allBuildingPrices.reduce((total, price) => total + price, 0);
+    const avgBuildingPrice = Number((totalBuildingPrices / allBuildingPrices.length).toFixed(1));
+    await firestore
+      .collection('building-prices')
+      .doc(today)
+      .set({ date, value: avgBuildingPrice, createdAt: admin.firestore.FieldValue.serverTimestamp() });
+  } else {
+    await firestore
+      .collection('building-prices')
+      .doc(today)
+      .set({ date, value: 0, createdAt: admin.firestore.FieldValue.serverTimestamp() });
+  }
+};
+
+export default calculateAvgPrice;
