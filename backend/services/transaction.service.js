228c7779f7fe96bf0bb608b84f445abd6e6a4857
jack <jack.nguyen@weareday.one>
2024-02-16 16:07:16 +0700

    chores: implement backend error

--- backend/services/transaction.service.js
+++ backend/services/transaction.service.js
@@ -567,7 +567,7 @@ export const claimToken = async ({ userId }) => {
   console.log('Claim token');
   const userSnapshot = await firestore.collection('user').doc(userId).get();
   const activeSeason = await getActiveSeason();
-  if (activeSeason.status !== 'open') throw new Error('Season ended');
+  if (activeSeason.status !== 'open') throw new Error('API error: Season ended');
 
   if (userSnapshot.exists) {
     const { address, tokenBalance } = userSnapshot.data();
