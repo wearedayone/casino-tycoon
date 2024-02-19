14079a4daa199ac0c952f25091e65d3a6f33aeb7
Ally Tran <h.anhtr15@gmail.com>
2024-02-19 08:51:17 +0700

    feat: fetch NFT price

--- cronjob/index.js
+++ cronjob/index.js
@@ -1,16 +1,26 @@
 import cron from 'node-cron';
 
 import { updateFIATPriceUniswapV2 } from './tasks/updateFIATPrice.js';
+import updateNFTPrice from './tasks/updateNFTPrice.js';
 import calculateAvgPrice from './tasks/calculateAvgPrice.js';
 import estimateGasPrice from './tasks/estimateGas.js';
 import getEthPrice from './tasks/getEthPrice.js';
 import environments from './utils/environments.js';
 
-const { CRON_CALCULATE_WORKER_BUILDING_PRICE, CRON_UPDATE_FIAT_PRICE, CRON_ESTIMATE_GAS_PRICE, CRON_GET_ETH_PRICE } =
-  environments;
+const {
+  CRON_CALCULATE_WORKER_BUILDING_PRICE,
+  CRON_UPDATE_FIAT_PRICE,
+  CRON_UPDATE_NFT_PRICE,
+  CRON_ESTIMATE_GAS_PRICE,
+  CRON_GET_ETH_PRICE,
+} = environments;
 
 cron.schedule(CRON_UPDATE_FIAT_PRICE, function () {
   updateFIATPriceUniswapV2();
+});
+
+cron.schedule(CRON_UPDATE_NFT_PRICE, function () {
+  updateNFTPrice();
 });
 
 cron.schedule(
