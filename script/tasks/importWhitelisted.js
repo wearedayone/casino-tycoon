c14b1763977a48c253c00d7b754ce134105af603
jack <jack.nguyen@weareday.one>
2024-03-04 17:06:46 +0700

    chores: import whitelisted existed issue

--- script/tasks/importWhitelisted.js
+++ script/tasks/importWhitelisted.js
@@ -10,24 +10,28 @@ const getActiveSeasonId = async () => {
 
 const checkExistedWhitelistedUser = async (username) => {
   try {
-    await firestore.collection('whitelisted').add({ username });
+    const existedDoc = await firestore.collection('whitelisted').where('username', '==', username).get();
+    if (existedDoc.empty) {
+      await firestore.collection('whitelisted').add({ username });
 
-    const snapshot = await firestore.collection('user').where('username', '==', username).limit(1).get();
-    if (!snapshot.empty) {
-      // update user isWhitelisted
-      await snapshot.docs[0].ref.update({ isWhitelisted: true });
+      const snapshot = await firestore.collection('user').where('username', '==', username).limit(1).get();
+      if (!snapshot.empty) {
+        // update user isWhitelisted
+        await snapshot.docs[0].ref.update({ isWhitelisted: true });
 
-      const seasonId = await getActiveSeasonId();
-      const gamePlaySnapshot = await firestore
-        .collection('gamePlay')
-        .where('seasonId', '==', seasonId)
-        .where('userId', '==', snapshot.docs[0].id)
-        .limit(1)
-        .get();
-      if (!gamePlaySnapshot.empty) {
-        await gamePlaySnapshot.docs[0].ref.update({ isWhitelisted: true });
+        const seasonId = await getActiveSeasonId();
+        const gamePlaySnapshot = await firestore
+          .collection('gamePlay')
+          .where('seasonId', '==', seasonId)
+          .where('userId', '==', snapshot.docs[0].id)
+          .limit(1)
+          .get();
+        if (!gamePlaySnapshot.empty) {
+          await gamePlaySnapshot.docs[0].ref.update({ isWhitelisted: true });
+        }
       }
     }
+
     console.error(`Completed importing user ${username}`);
   } catch (err) {
     console.error(`Error while importing user ${username}`);
