20ffc70fb7053e61201e735ddfea26e6640c09de
Ally Tran <h.anhtr15@gmail.com>
2024-03-15 20:52:14 +0700

    chore: import whitelist script with case checking

--- script/tasks/importWhitelisted.js
+++ script/tasks/importWhitelisted.js
@@ -1,5 +1,5 @@
 import { firestore } from '../configs/admin.config.js';
-import whitelisted from '../assets/jsons/whitelisted.json' assert { type: 'json' };
+import lastedWhitelisted from '../assets/jsons/whitelisted.json' assert { type: 'json' };
 
 const getActiveSeasonId = async () => {
   const snapshot = await firestore.collection('system').doc('default').get();
@@ -8,41 +8,59 @@ const getActiveSeasonId = async () => {
   return configs.activeSeasonId;
 };
 
-const checkExistedWhitelistedUser = async (username) => {
-  try {
-    const existedDoc = await firestore.collection('whitelisted').where('username', '==', username).get();
-    if (existedDoc.empty) {
-      await firestore.collection('whitelisted').add({ username });
+const main = async () => {
+  console.log('start import whitelisted users');
+  const seasonId = await getActiveSeasonId();
+  const whitelistSnapshot = await firestore.collection('whitelisted').get();
+  const usersSnapshot = await firestore.collection('user').get();
 
-      const snapshot = await firestore.collection('user').where('username', '==', username).limit(1).get();
-      if (!snapshot.empty) {
-        // update user isWhitelisted
-        await snapshot.docs[0].ref.update({ isWhitelisted: true });
+  const currentWhitelist = whitelistSnapshot.docs.map((doc) => ({
+    username: doc.data().username,
+    usernameLowercase: doc.data().username.toLowerCase(),
+  }));
 
-        const seasonId = await getActiveSeasonId();
-        const gamePlaySnapshot = await firestore
-          .collection('gamePlay')
-          .where('seasonId', '==', seasonId)
-          .where('userId', '==', snapshot.docs[0].id)
-          .limit(1)
-          .get();
-        if (!gamePlaySnapshot.empty) {
-          await gamePlaySnapshot.docs[0].ref.update({ isWhitelisted: true });
+  const users = usersSnapshot.docs.map((doc) => ({
+    ref: doc.ref,
+    id: doc.id,
+    username: doc.data().username,
+    isWhitelisted: doc.data().isWhitelisted,
+    usernameLowercase: doc.data().username.toLowerCase(),
+  }));
+
+  for (let username of lastedWhitelisted) {
+    try {
+      const existedWhitelist = currentWhitelist.find(
+        ({ usernameLowercase }) => usernameLowercase === username.toLowerCase()
+      );
+      if (!existedWhitelist) {
+        const user = users.find(({ usernameLowercase }) => usernameLowercase === username.toLowerCase());
+
+        // use user's twitter username if found
+        const correctUsername = user?.username || username;
+        await firestore.collection('whitelisted').add({ username: correctUsername });
+
+        if (user) {
+          // update user isWhitelisted
+          await user.ref.update({ isWhitelisted: true });
+
+          const gamePlaySnapshot = await firestore
+            .collection('gamePlay')
+            .where('seasonId', '==', seasonId)
+            .where('userId', '==', user.id)
+            .limit(1)
+            .get();
+          if (!gamePlaySnapshot.empty) {
+            await gamePlaySnapshot.docs[0].ref.update({ isWhitelisted: true });
+          }
         }
       }
+
+      console.error(`Completed importing user ${username}`);
+    } catch (err) {
+      console.error(`Error while importing user ${username}`);
+      console.error(err);
     }
-
-    console.error(`Completed importing user ${username}`);
-  } catch (err) {
-    console.error(`Error while importing user ${username}`);
-    console.error(err);
   }
-};
-
-const main = async () => {
-  console.log('start import whitelist user');
-  const promises = whitelisted.map((username) => checkExistedWhitelistedUser(username));
-  await Promise.all(promises);
   console.log('done');
 };
 
