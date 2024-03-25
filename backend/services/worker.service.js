0c518ae419a5dfdb6edda01d36fc33fccdc2faf3
huyhn310 <huyhn310@gmail.com>
2024-03-25 19:14:54 +0700

    fix claim token error

--- backend/services/worker.service.js
+++ backend/services/worker.service.js
@@ -1,6 +1,7 @@
 import { Contract } from '@ethersproject/contracts';
 import { Wallet } from '@ethersproject/wallet';
-import { ethers, parseEther } from 'ethers';
+import { formatEther, parseEther as UnitPasteEther } from '@ethersproject/units';
+import { ethers, isError, parseEther } from 'ethers';
 import { getParsedEthersError } from '@enzoferey/ethers-error-parser';
 import { Utils } from 'alchemy-sdk';
 
@@ -56,119 +57,219 @@ export const decodeTokenTxnLogs = async (name, log) => {
 
 export const claimToken = async ({ address, amount }) => {
   let txnHash = '';
-  try {
-    console.log({ WORKER_WALLET_PRIVATE_KEY });
-    logger.info('start claimToken');
-    logger.info({ address, amount });
-    const ethersProvider = await alchemy.config.getProvider();
-    // const gasPrice = await ethersProvider.getGasPrice();
-    const workerWallet = await getWorkerWallet();
-    const tokenContract = await getTokenContract(workerWallet);
-    logger.info('start Transaction:');
-    const tx = await tokenContract.mint(address, amount);
-    logger.info('Transaction:' + tx.hash);
-    txnHash = tx.hash;
-    const receipt = await tx.wait();
+  let isSuccess = false;
+  let retry = 0;
 
-    if (receipt.status !== 1) {
-      logger.info(`error: ${JSON.stringify(receipt)}`);
-      logger.error(`error: ${JSON.stringify(receipt)}`);
-      throw new Error(`API error: Txn failed`);
-    }
+  while (!isSuccess && retry < 10) {
+    try {
+      logger.info(`Start claimToken - ${retry++} times`);
+      logger.info({ address, amount });
+      const ethersProvider = await alchemy.config.getProvider();
+      const workerWallet = await getWorkerWallet();
+      const tokenContract = await getTokenContract(workerWallet);
 
-    return { txnHash, status: 'Success' };
-  } catch (err) {
-    const newError = getParsedEthersError(err);
-    const regex = /(execution reverted: )([A-Za-z\s])*/;
-    if (newError.context) {
-      const message = newError.context.match(regex);
-      if (message) {
-        const error = new Error(message[0]);
-        logger.error(error.message);
+      const baseGasPrice = await ethersProvider.getGasPrice();
+      const gasPrice = (baseGasPrice.toBigInt() * BigInt(Math.round(Math.pow(1.2, retry) * 1000000))) / BigInt(1000000);
+
+      logger.info(`Start claimToken transaction with gasPrice: ${JSON.stringify(gasPrice)}`);
+      const tx = await tokenContract.mint(address, amount, { gasPrice });
+      logger.info('Transaction claimToken:' + tx.hash);
+      txnHash = tx.hash;
+      const receipt = await tx.wait();
+
+      if (receipt.status !== 1) {
+        logger.info(`error: ${JSON.stringify(receipt)}`);
+        logger.error(`error: ${JSON.stringify(receipt)}`);
+        throw new Error(`API error: Txn failed`);
       }
-    } else {
-      logger.error(err.message);
-    }
+      isSuccess = true;
+      logger.info(`Finished claimToken transaction with txnHash: ${txnHash}`);
+      return { txnHash, status: 'Success' };
+    } catch (err) {
+      try {
+        logger.info(`Error claimToken transaction with txnHash: ${txnHash}`);
+        if (isError(err, 'UNPREDICTABLE_GAS_LIMIT')) {
+          logger.info('UNPREDICTABLE_GAS_LIMIT');
+        } else if (isError(err, 'TRANSACTION_REPLACED')) {
+          logger.info('TRANSACTION_REPLACED');
+        } else if (isError(err, 'REPLACEMENT_UNDERPRICED')) {
+          logger.info('REPLACEMENT_UNDERPRICED');
+        } else if (isError(err, 'NONCE_EXPIRED')) {
+          logger.info('NONCE_EXPIRED');
+        } else if (isError(err, 'NETWORK_ERROR')) {
+          logger.info('NETWORK_ERROR');
+        } else if (isError(err, 'SERVER_ERROR')) {
+          logger.info('SERVER_ERROR');
+        } else if (isError(err, 'INSUFFICIENT_FUNDS')) {
+          logger.info('INSUFFICIENT_FUNDS');
+        } else if (isError(err, 'ACTION_REJECTED')) {
+          logger.info('ACTION_REJECTED');
+        } else {
+          logger.info('Other Error');
+        }
 
-    return { txnHash: txnHash || '', status: 'Failed' };
+        logger.info(`Unsuccessful txn: ${JSON.stringify(err)}`);
+        logger.error(`Unsuccessful txn: ${JSON.stringify(err)}`);
+
+        const newError = getParsedEthersError(err);
+        const regex = /(execution reverted: )([A-Za-z\s])*/;
+        if (newError.context) {
+          const message = newError.context.match(regex);
+          if (message) {
+            const error = new Error(message[0]);
+            logger.error(error.message);
+          }
+        } else {
+          logger.error(err.message);
+        }
+      } catch {
+        logger.info('Error happened at claimToken catch');
+      }
+    }
   }
+  return { txnHash: txnHash || '', status: 'Failed' };
 };
 
 export const claimTokenBatch = async ({ addresses, amounts }) => {
   let txnHash = '';
-  try {
-    logger.info('start claimTokenBatch');
-    logger.info({ addresses, amounts });
-    console.log({ WORKER_WALLET_PRIVATE_KEY });
-    const workerWallet = await getWorkerWallet();
-    const tokenContract = await getTokenContract(workerWallet);
-    logger.info('start Transaction:');
-    const tx = await tokenContract.batchMint(addresses, amounts);
-    txnHash = tx.hash;
-    logger.info('Transaction:' + tx.hash);
-    const receipt = await tx.wait();
+  let isSuccess = false;
+  let retry = 0;
+  const ethersProvider = await alchemy.config.getProvider();
+  while (!isSuccess && retry < 10) {
+    try {
+      logger.info(`Start claimTokenBatch. Retry ${retry++} times`);
+      logger.info({ addresses, amounts });
+      const workerWallet = await getWorkerWallet();
+      const tokenContract = await getTokenContract(workerWallet);
 
-    if (receipt.status !== 1) {
-      logger.error(`Unsuccessful txn: ${JSON.stringify(receipt)}`);
-      throw new Error(`API error: Txn failed`);
-    }
+      const baseGasPrice = await ethersProvider.getGasPrice();
+      const gasPrice = (baseGasPrice.toBigInt() * BigInt(Math.round(Math.pow(1.2, retry) * 1000000))) / BigInt(1000000);
+      logger.info(`Start claimTokenBatch transaction with gasPrice: ${JSON.stringify(gasPrice)}`);
+      const tx = await tokenContract.batchMint(addresses, amounts, { gasPrice });
+      txnHash = tx.hash;
+      logger.info('ClaimTokenBatch Transaction:' + tx.hash);
+      const receipt = await tx.wait();
 
-    return { txnHash, status: 'Success' };
-  } catch (err) {
-    console.error(err);
-    const newError = getParsedEthersError(err);
-    const regex = /(execution reverted: )([A-Za-z\s])*/;
-    if (newError.context) {
-      const message = newError.context.match(regex);
-      if (message) {
-        const error = new Error(message[0]);
-        logger.error(error.message);
+      if (receipt.status !== 1) {
+        logger.error(`Unsuccessful txn: ${JSON.stringify(receipt)}`);
+        throw new Error(`API error: Txn failed`);
       }
-    } else {
-      logger.error(err.message);
-    }
+      isSuccess = true;
+      logger.info(`Finished ClaimTokenBatch transaction with txnHash: ${txnHash}`);
+      return { txnHash, status: 'Success' };
+    } catch (err) {
+      try {
+        logger.info(`Error claimTokenBatch transaction with txnHash: ${txnHash}`);
+        if (isError(err, 'UNPREDICTABLE_GAS_LIMIT')) {
+          logger.info('UNPREDICTABLE_GAS_LIMIT');
+        } else if (isError(err, 'TRANSACTION_REPLACED')) {
+          logger.info('TRANSACTION_REPLACED');
+        } else if (isError(err, 'REPLACEMENT_UNDERPRICED')) {
+          logger.info('REPLACEMENT_UNDERPRICED');
+        } else if (isError(err, 'NONCE_EXPIRED')) {
+          logger.info('NONCE_EXPIRED');
+        } else if (isError(err, 'NETWORK_ERROR')) {
+          logger.info('NETWORK_ERROR');
+        } else if (isError(err, 'SERVER_ERROR')) {
+          logger.info('SERVER_ERROR');
+        } else if (isError(err, 'INSUFFICIENT_FUNDS')) {
+          logger.info('INSUFFICIENT_FUNDS');
+        } else if (isError(err, 'ACTION_REJECTED')) {
+          logger.info('ACTION_REJECTED');
+        } else {
+          logger.info('Other Error');
+        }
 
-    return { txnHash, status: 'Failed' };
+        logger.info(`Unsuccessful txn: ${JSON.stringify(err)}`);
+        logger.error(`Unsuccessful txn: ${JSON.stringify(err)}`);
+
+        const newError = getParsedEthersError(err);
+        const regex = /(execution reverted: )([A-Za-z\s])*/;
+        if (newError.context) {
+          const message = newError.context.match(regex);
+          if (message) {
+            logger.error(message[0]);
+          }
+        } else {
+          logger.error(err.message);
+        }
+      } catch {
+        logger.info('Error happened at claimTokenBatch catch');
+      }
+    }
   }
+  return { txnHash, status: 'Failed' };
 };
 
 export const burnNFT = async ({ addresses, ids, amounts }) => {
   let txnHash = '';
-  try {
-    logger.info('start burnNFT');
-    logger.info({ addresses, ids, amounts });
-    const ethersProvider = await alchemy.config.getProvider();
-    const gasPrice = await ethersProvider.getGasPrice();
-    const workerWallet = await getWorkerWallet();
-    const gameContract = await getGameContract(workerWallet);
-    logger.info('start Transaction:');
-    const tx = await gameContract.burnNFT(addresses, ids, amounts, { gasPrice });
-    txnHash = tx.hash;
-    logger.info('Transaction:' + tx.hash);
-    const receipt = await tx.wait();
+  let isSuccess = false;
+  let retry = 0;
+  const ethersProvider = await alchemy.config.getProvider();
+  while (!isSuccess && retry < 10) {
+    try {
+      logger.info(`Start burnNFT. Retry ${retry++} times`);
+      logger.info({ addresses, ids, amounts });
+      const workerWallet = await getWorkerWallet();
+      const gameContract = await getGameContract(workerWallet);
 
-    if (receipt.status !== 1) {
-      logger.error(`Unsuccessful txn: ${JSON.stringify(receipt)}`);
-      throw new Error(`API error: Txn failed`);
-    }
+      const baseGasPrice = await ethersProvider.getGasPrice();
+      const gasPrice = (baseGasPrice.toBigInt() * BigInt(Math.round(Math.pow(1.2, retry) * 1000000))) / BigInt(1000000);
+      logger.info(`Start burnNFT transaction with gasPrice: ${JSON.stringify(gasPrice)}`);
+      const tx = await gameContract.burnNFT(addresses, ids, amounts, { gasPrice });
+      txnHash = tx.hash;
+      logger.info('BurnNFT Transaction:' + tx.hash);
+      const receipt = await tx.wait();
 
-    return { txnHash, status: 'Success' };
-  } catch (err) {
-    console.error(err);
-    const newError = getParsedEthersError(err);
-    const regex = /(execution reverted: )([A-Za-z\s])*/;
-    if (newError.context) {
-      const message = newError.context.match(regex);
-      if (message) {
-        const error = new Error(message[0]);
-        logger.error(error.message);
+      if (receipt.status !== 1) {
+        logger.error(`Unsuccessful txn: ${JSON.stringify(receipt)}`);
+        throw new Error(`API error: Txn failed`);
       }
-    } else {
-      logger.error(err.message);
-    }
+      isSuccess = true;
+      logger.info(`Finished burnNFT transaction with txnHash: ${txnHash}`);
+      return { txnHash, status: 'Success' };
+    } catch (err) {
+      try {
+        logger.info(`Error burnNFT transaction with txnHash: ${txnHash}`);
+        if (isError(err, 'UNPREDICTABLE_GAS_LIMIT')) {
+          logger.info('UNPREDICTABLE_GAS_LIMIT');
+        } else if (isError(err, 'TRANSACTION_REPLACED')) {
+          logger.info('TRANSACTION_REPLACED');
+        } else if (isError(err, 'REPLACEMENT_UNDERPRICED')) {
+          logger.info('REPLACEMENT_UNDERPRICED');
+        } else if (isError(err, 'NONCE_EXPIRED')) {
+          logger.info('NONCE_EXPIRED');
+        } else if (isError(err, 'NETWORK_ERROR')) {
+          logger.info('NETWORK_ERROR');
+        } else if (isError(err, 'SERVER_ERROR')) {
+          logger.info('SERVER_ERROR');
+        } else if (isError(err, 'INSUFFICIENT_FUNDS')) {
+          logger.info('INSUFFICIENT_FUNDS');
+        } else if (isError(err, 'ACTION_REJECTED')) {
+          logger.info('ACTION_REJECTED');
+        } else {
+          logger.info('Other Error');
+        }
 
-    return { txnHash, status: 'Failed' };
+        logger.info(`Unsuccessful txn: ${JSON.stringify(err)}`);
+        logger.error(`Unsuccessful txn: ${JSON.stringify(err)}`);
+        const newError = getParsedEthersError(err);
+        const regex = /(execution reverted: )([A-Za-z\s])*/;
+        if (newError.context) {
+          const message = newError.context.match(regex);
+          if (message) {
+            const error = new Error(message[0]);
+            logger.error(error.message);
+          }
+        } else {
+          logger.error(err.message);
+        }
+      } catch {
+        logger.info('Error happened at burnNFT catch');
+      }
+    }
   }
+  return { txnHash, status: 'Failed' };
 };
 
 export const burnGoon = async ({ addresses, amounts }) => {
