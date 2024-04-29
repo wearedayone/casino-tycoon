31b28957e96735e7bbd4b40f50020770f609e7f8
Ally Tran <h.anhtr15@gmail.com>
2024-04-29 12:55:39 +0700

    fix: tutorial goes back to step 2 if bg is clicked

--- app/src/pages/Game/components/tutorials/Step1.js
+++ app/src/pages/Game/components/tutorials/Step1.js
@@ -6,12 +6,12 @@ import TutorialCharacter from './TutorialCharacter';
 const { width, height } = configs;
 
 class Step1 extends Phaser.GameObjects.Container {
-  constructor(scene, onNext) {
+  constructor(scene) {
     super(scene, 0, 0);
 
     this.setVisible(false);
 
-    this.character = new TutorialCharacter(scene, width / 2, height - 650, 'tutorial-1', onNext);
+    this.character = new TutorialCharacter(scene, width / 2, height - 650, 'tutorial-1');
     this.add(this.character);
   }
 }
